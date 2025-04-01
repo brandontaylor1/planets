15d1b8706ee0d7090e56e9da56f86d6dbebecf4e
brandontaylor1 <brandon@thecreativekingdomco.com>
2025-04-01 10:39:48 -0500

    Add Farenheit button functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,51 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import InfoBox from './InfoBox'
+import TitleModal from './TitleModal'
+
 import { PlanetsContext } from '../../context/PlanetsContext'
 
 const Footer = () => {
 
 const { selectedPlanet } = useContext(PlanetsContext)
 
-// const { temp, setTemp } = useState('') 
+const [ temp, setTemp ] = useState('')
+const [ isModalVisible, setIsModalVisible ] = useState(false)
 
-// const farenheit = () => {
-//     return selectedPlanet.temperature * 9/5 + 32
-// }
+// Function to convert temperature from Celsius to Fahrenheit
+const convertToFahrenheit = (celsius) => {
+    const celsiusRegEx = /-?[0-9]+/
+    const celsiusMatch = celsius.match(celsiusRegEx)
+    console.log(celsiusMatch)
+    return celsiusMatch[0] * 9/5 + 32
 
-// const handleButtonClick = (temp) => {
-//     setTemp(!temp) 
-// }
+}
 
+const handleButtonClick = () => {
+    setTemp(convertToFahrenheit(selectedPlanet?.temperature))
+}
+
+useEffect(() => {
+    setTemp('')
+
+}, [selectedPlanet])
+
+const planetName = selectedPlanet?.name?.toLowerCase() || '';
+
+const planetColors = {
+    mercury: 'bg-[var(--color-mercury)]',
+    venus: 'bg-[var(--color-venus)]',
+    earth: 'bg-[var(--color-earth)]',
+    mars: 'bg-[var(--color-mars)]',
+    jupiter: 'bg-[var(--color-jupiter)]',
+    saturn: 'bg-[var(--color-saturn)]',
+    uranus: 'bg-[var(--color-uranus)]',
+    neptune: 'bg-[var(--color-neptune)]',
+  };
+
+  const planetColor = planetColors[planetName] || 'bg-[var(--color-primary-white)]';
+
+  
   return (
     <footer className='w-full text-white h-[auto]'>
         <div className="w-100%] h-[100%] flex flex-row items-center justify-center">
@@ -32,17 +61,23 @@ const { selectedPlanet } = useContext(PlanetsContext)
             items-left justify-center gap-4 p-10">
                 {selectedPlanet && <InfoBox title="Radius" content={selectedPlanet.radius} />}
             </div>
-            <div className="w-[40%] h-[100%] flex flex-col 
-            items-left justify-center gap-4 p-10">
-                {selectedPlanet && <InfoBox title="Average Temp." content={selectedPlanet.temperature}/> }
-                
-                {/* NEED TO FIX THE BUTTON COMPONENT */}
-                {/* <button
-                    onClick={() => handleButtonClick()}
-                >Click for Farenheit</button> */}
+                <div className="w-[40%] h-[100%] flex flex-col 
+                items-left justify-center gap-4 p-10">
+                    {selectedPlanet && 
+                    <InfoBox 
+                        title="Average Temp." 
+                        content={temp !== '' ? `${temp}°F` : selectedPlanet.temperature}
+                        /> }
+
+                {!selectedPlanet ? (<TitleModal />) : (
+                    <button 
+                    className={`body uppercase font-medium ${planetColor} text-white font-bold py-2 px-4 rounded`} 
+                    onClick={() => handleButtonClick()}>Click For Farenheit </button>) 
+                }   
+                </div>
+                <div>
             </div>
         </div>
-    
     </footer>
   )
 }
